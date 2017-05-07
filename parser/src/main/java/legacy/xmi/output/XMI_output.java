9e46ec0487e9201973e1a12a206bd1ad1735b001
Alexander Vasileyko <alexnodejs@yahoo.com>
2017-05-07 04:22:36 +0300

    Update thesis

--- parser/src/main/java/legacy/xmi/output/XMI_output.java
+++ parser/src/main/java/legacy/xmi/output/XMI_output.java
@@ -10,26 +10,19 @@ import legacy.xmi.root.elements.XMI;
 
 public class XMI_output {
 
-	public static void WriteToXMIFile(String _filepath,XMI _xmi)
-	{
-		
-		
+	public static void WriteToXMIFile(String _filepath,XMI _xmi) {
 		try {
-			 
 			File file = new File(_filepath);
 			JAXBContext jaxbContext = JAXBContext.newInstance(XMI.class);
 			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 	 
 			// output pretty printed
 			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-	 
 			jaxbMarshaller.marshal(_xmi, file);
 			jaxbMarshaller.marshal(_xmi, System.out);
-	 
-		      } catch (JAXBException e) {
+
+		} catch (JAXBException e) {
 			e.printStackTrace();
-		      }
+		}
 	}
-	
-	
 }
